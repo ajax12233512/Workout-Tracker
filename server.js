490748de8d503cfa82637f2a4b32845d78b3c358
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2021-12-16 21:47:44 -0600

    fixed server port to include process.env.port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const app = express();
 const path = require('path');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'))
 
