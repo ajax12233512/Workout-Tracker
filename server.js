3b899e655d003d1144ec6812f3b0063209dc3501
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2021-12-16 21:46:19 -0600

    deploying database

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const app = express();
+const path = require('path');
+
+const PORT = 3001;
+
+app.use(express.static('public'))
+
+//Database connections
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/aqueous-beyond-96443", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+
+});
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/exercise', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/public/exercise.html'))
+})
+
+app.get('/stats', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/public/stats.html'))
+})
+
+app.listen(PORT, () =>{console.log('Server listening on http://localhost:3001')})
