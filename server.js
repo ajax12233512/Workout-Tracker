41c64a4e106ff976c90caa4f94bef7fbdab3786a
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2022-01-11 14:03:19 -0600

    final commit. heroku not able to function

--- server.js
+++ server.js
@@ -13,13 +13,17 @@ app.use(express.static('public'));
 app.use(api);
 
 //Database connections
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/aqueous-beyond-96443", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-
 });
+
+// console.log(connection);
+
+
 
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, '/index.html'))
